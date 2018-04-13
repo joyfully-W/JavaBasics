0b98d8689e7b2f1e8821d7fde0c7541c080bb694
Same-time <taoge1284267900@163.com>
2018-04-14 00:02:55 +0800

    提交文件

---
+++ 99乘法表/demo99.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class demosxh {
+	public static void main(String[] args){
+
+		//Ñ­»·
+		for(int i=1;i<10;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print(j+"*"+i+"="+i*j+"  ");
+			}
+			//»»ÐÐ
+			System.out.println();
+		}
+		
+	}
+}
