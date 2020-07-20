86cb8625d98a892d0cc2c34af4317a437034f807
Same-time <taoge1284267900@163.com>
2020-07-20 17:32:56 +0800

    内部类

---
+++ com/joyfully/InnerclassLearn/AnonymousInnerClass/AICInterface.java
@@ -0,0 +1,5 @@
+package com.joyfully.InnerclassLearn.AnonymousInnerClass;
+
+public interface AICInterface {
+	void method(); // 抽象方法
+}
---
+++ com/joyfully/InnerclassLearn/PartInnerclass/PartInnerclassMain.java
@@ -0,0 +1,10 @@
+package com.joyfully.InnerclassLearn.PartInnerclass;
+
+public class PartInnerclassMain {
+
+    public static void main(String[] args) {
+        Outer obj = new Outer();
+        obj.methodOuter();
+    }
+
+}
