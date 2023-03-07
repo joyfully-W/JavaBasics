ecf90c00f01f96d675289141a23910c9327b45d5
Joyfully <584974245@qq.com>
2023-03-07 10:38:10 +0800

    代码修改

---
+++ cn/joyfully/AbstractLearn/AbstractMain.java
@@ -0,0 +1,12 @@
+package cn.joyfully.AbstractLearn;
+
+public class AbstractMain {
+    public static void main(String[] args) {
+
+        //错误写法！不能直接创建抽象类对象
+        //Animal animal = new Animal();
+
+        Cat cat = new Cat();
+        cat.eat();
+    }
+}
---
+++ cn/joyfully/AbstractLearn/Cat.java
@@ -0,0 +1,9 @@
+package cn.joyfully.AbstractLearn;
+
+public class Cat extends Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("The Cat eat the fish!");
+    }
+}
