991fd7c82e9da79090e07871b917beb32db2313f
Joyfully <584974245@qq.com>
2023-03-15 09:07:24 +0800

    超时demo修改

---
+++ cn/joyfully/demo/market/Fitem.java
@@ -0,0 +1,15 @@
+package cn.joyfully.demo.market;
+
+public class Fitem {
+    //商品号
+    int ID;
+    //商品名称
+    String name;
+    //商品价格
+    double price;
+    //数量
+    int number;
+    //总金额
+    double money;
+
+}
