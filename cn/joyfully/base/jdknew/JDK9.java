0a0e37e610b705dd22b77ea766bcb1c99e38eb3b
Joyfully <584974245@qq.com>
2023-04-18 17:27:18 +0800

    jdk9 新特性

--- cn/joyfully/base/jdknew/JDK9.java
+++ cn/joyfully/base/jdknew/JDK9.java
@@ -1,10 +1,11 @@
-package com.itheima.demo04.JDK9;
+package cn.joyfully.base.jdknew;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/*
+public class JDK9 {
+    /*
     JDK9的新特性:
         List接口,Set接口,Map接口:里边增加了一个静态的方法of,可以给集合一次性添加多个元素
         static <E> List<E> of​(E... elements)
@@ -14,8 +15,7 @@ import java.util.Set;
         1.of方法只适用于List接口,Set接口,Map接口,不适用于接接口的实现类
         2.of方法的返回值是一个不能改变的集合,集合不能再使用add,put方法添加元素,会抛出异常
         3.Set接口和Map接口在调用of方法的时候,不能有重复的元素,否则会抛出异常
- */
-public class JDK9_of {
+     */
     public static void main(String[] args) {
         List<String> list = List.of("a", "b", "a", "c", "d");
         System.out.println(list);//[a, b, a, c, d]
